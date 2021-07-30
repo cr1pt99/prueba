12bffc73a2d17c80d6a55adfb58051706c8c5635
OscarHdzLinux <atackevil9922@gmail.com>
2021-07-30 18:12:19 -0500

    se agrego texto 2 develoop1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,6 +56,7 @@ body {
     </head>
     <body class="antialiased">
         <h2 style="color: white">se agrego text develop1</h2>
+        <h3>texto2222</h3>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
