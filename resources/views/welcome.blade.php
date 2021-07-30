e69cfc350d8e2e983427ef42f8dd76e12a67bf54
OscarHdzLinux <atackevil9922@gmail.com>
2021-07-30 18:20:44 -0500

    se agrego un texto mas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,6 +57,7 @@ body {
     <body class="antialiased">
         <h2 style="color: white">se agrego text develop1</h2>
         <h3>texto2222</h3>
+        <h1>tercer commit develop1</h1>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
